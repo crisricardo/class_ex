f1f328facfdb4d371b41c7d7bde5edd4e3db6e2d
crisricardo <2024381@student.cct.ie>
2024-12-12 13:18:41 +0000

    commit
    commit

--- myex.py
+++ myex.py
@@ -1,13 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+import streamlit as st
 
-\f0\fs24 \cf0 import streamlit as st \
-\
-st.title(\'93My New App\'94)\
-st.write(\'93This is my new Streamlit App Cristian Ricardo\'94)\
-\
-st.write(\'93This is a change goats\'94)}
+st.title("My New App")
+st.write("This is my new Streamlit App Cristian Ricardo")
+
+st.write("This is a change goats")
